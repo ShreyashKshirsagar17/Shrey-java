c905b4a4976d0e5738ba56b769ddd40132b11130
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-03-31 23:37:32 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pattern3 {
+    public static void main(String []args)
+    {
+        System.out.println("Enter the value for what you want to print");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
