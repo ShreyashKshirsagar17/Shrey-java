5d46f6366c6dc5151b2dcba6924661110d637323
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-04-04 01:17:22 +0530

    Add files via upload

---
+++ Pattern9.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Pattern9 {
+    public static void main(String[]args){
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            int space =2*(n-1);
+        
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+        
+    }
+}
+}
