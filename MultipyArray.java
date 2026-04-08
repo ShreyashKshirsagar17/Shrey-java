688024995ff859c3872b6d577ea8b1edb4f63fc2
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-04-08 10:50:22 +0530

    Add files via upload

---
+++ MultipyArray.java
@@ -0,0 +1,33 @@
+package practice1;
+
+public class MultipyArray {
+
+	public static void main(String[] args) {
+		int A[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		int B[][]= {{1,0,0},{0,1,0},{0,0,1}};
+		int C[][]=new int [3][3];
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				C[i][j]=0;
+				for(int k=0;k<3;k++)
+				{
+					C[i][j]=C[i][j]+A[i][k]+B[k][j];
+				}
+			}
+		}
+		for(int x[]:C)
+		{
+			for(int y:x)
+			{
+				System.out.print(y+" ,");
+			}
+			System.out.println(" ");
+		}
+		
+
+	}
+
+}
