fc1497e7d0f07d6170dec4678b7fb24f20a2d0c9
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-04-13 14:18:37 +0530

    Add files via upload

---
+++ child1.java
@@ -0,0 +1,15 @@
+package practice1;
+
+public class child1 {
+
+	public static void main(String []args)
+	{
+		child ch=new child ();
+		ch.display();
+		System.out.println(ch.show());
+		overriding  ov=new child();
+		ov.display();
+		
+		
+}
+}
