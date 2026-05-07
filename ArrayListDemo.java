46402a99f58706bf76ba59f450a8efbd99a8217a
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-05-07 23:50:47 +0530

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,20 @@
+package MatrixRotation;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        System.out.println("Elements in ArrayList:");
+
+        for(String s : list) {
+            System.out.println(s);
+        }
+    }
+}
