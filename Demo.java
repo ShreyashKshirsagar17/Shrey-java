cb3e39f0642753474d1e07a3b6869a0b6befdb83
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-05-08 23:29:06 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,22 @@
+package MatrixRotation;
+
+class Addition {
+
+    void add(int a, int b) {
+        System.out.println("Sum = " + (a + b));
+    }
+
+    void add(int a, int b, int c) {
+        System.out.println("Sum = " + (a + b + c));
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Addition obj = new Addition();
+
+        obj.add(10, 20);
+        obj.add(10, 20, 30);
+    }
+}
