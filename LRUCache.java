bcea5d35775236f3d7a66a60e087a1b4ce71dfce
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-06-01 23:35:21 +0530

    Add files via upload

---
+++ LRUCache.java
@@ -0,0 +1,93 @@
+package dsa;
+
+import java.util.HashMap;
+
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void addNode(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToFront(Node node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+
+        Node node = map.get(key);
+        moveToFront(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            moveToFront(node);
+        } else {
+
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+                removeNode(lru);
+                map.remove(lru.key);
+            }
+
+            Node newNode = new Node(key, value);
+            addNode(newNode);
+            map.put(key, newNode);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 10);
+        cache.put(2, 20);
+
+        System.out.println(cache.get(1));
+
+        cache.put(3, 30);
+
+        System.out.println(cache.get(2));
+    }
+}
